a7fbd66e77e3685733fdcb045eb9615d154813ed
Jhanniel <34291983+jhonniel@users.noreply.github.com>
2021-09-27 22:09:56 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSeach from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyByPzvYHAQz1Q_WTLDCU6RVs7K3Jawa4uc';
 
-const App = () => {
-	return (
-	<div>
-		<SearchBar />
-	</div>
-	);
+
+
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		
+		this.state = { videos: [] };
+		
+		YTSeach({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });	
+		});
+	}
+	
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
