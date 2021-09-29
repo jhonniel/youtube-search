c6647959f3110f2c712ecafbe4cab5526605f852
Jhanniel <34291983+jhonniel@users.noreply.github.com>
2021-09-29 12:35:32 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -14,19 +14,32 @@ class App extends Component {
 	constructor(props){
 		super(props);
 		
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 		
-		YTSeach({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });	
+		this.videoSearch('java');
+	}
+	
+	videoSearch(term){
+		YTSeach({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});	
 		});
+		
 	}
 	
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 		);
 	}
